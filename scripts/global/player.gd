c9699e9e15f34e9866207388c0fe3e89a40b31b7
Matheus Almeida <85509639+m2atheus@users.noreply.github.com>
2026-04-16 08:44:59 -0300

    Revert "script do player"

--- scripts/global/player.gd
+++ scripts/global/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-<<<<<<< Updated upstream
 const SPEED = 200.0
 
 func _physics_process(delta: float) -> void:
@@ -13,28 +12,5 @@ func _physics_process(delta: float) -> void:
 		velocity = direction.normalized() * SPEED
 	else:
 		velocity = Vector2.ZERO
-=======
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -490.0
-
-
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if direction > 0:
-		anim.flip_h = false
-	elif direction < 0:
-		anim.flip_h = true
->>>>>>> Stashed changes
 
 	move_and_slide()
