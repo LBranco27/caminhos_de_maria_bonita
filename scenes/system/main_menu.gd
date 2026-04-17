039003a43f19f5b60b75067a2dc7855048ceadcd
Lucas Branco Alves de Melo <27lucasbranco27@gmail.com>
2026-04-16 21:44:04 -0300

    adjust transition, fix camera offset, fix player position, improve transition

--- scenes/system/main_menu.gd
+++ scenes/system/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	SceneManager.change_scene("res://scenes/path0.tscn")
+	SceneManager.change_scene("res://scenes/main/camp/acampamento.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
